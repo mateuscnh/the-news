ceac3921f8af1ff881432b61f5aac41909d17071
Mateus Cunha <mtscnh2@gmail.com>
2020-06-03 20:45:08 -0300

    reorganization

---
+++ frontend/src/js/header.js
@@ -0,0 +1,23 @@
+let menu = document.getElementById('menu');
+let navBar = document.getElementById('navbar');
+menu.addEventListener('click', () => {
+    document.body.style.overflow = 'hidden';
+    navBar.style.visibility = 'visible';
+});
+
+navBar.addEventListener('click', () => {
+    navBar.style.visibility = 'hidden';
+    document.body.style.overflow = 'visible';
+});
+
+let inputSearch = document.getElementById('inputSearch');
+
+inputSearch.addEventListener('focus', () => {
+    let search = document.getElementById('search');
+    search.style.width = '230px';
+})
+
+inputSearch.addEventListener('blur', () => {
+    let search = document.getElementById('search');
+    search.style.width = '130px';
+})
--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,3 +1,21 @@
+let newsWrap = document.getElementsByClassName('newsWrap');
+
+for (let index = 0; index < newsWrap.length; index++) {
+    newsWrap[index].addEventListener('mouseenter', () => {
+        newsWrap[index].style.backgroundSize = '120%';
+    })
+    newsWrap[index].addEventListener('mouseleave', () => {
+        newsWrap[index].style.backgroundSize = '115%';
+    })
+}
+
+let imgPost = document.querySelectorAll('#imgPost');
+imgPost[0].style.background = "url('images/news1.jpg') no-repeat center";
+imgPost[0].style.backgroundSize = 'contain';
+imgPost[1].style.background = "url('images/news2.jpg') no-repeat center";
+imgPost[1].style.backgroundSize = 'contain';
+imgPost[2].style.background = "url('images/news3.jpg') no-repeat center";
+imgPost[2].style.backgroundSize = 'contain';
 
 function createNews(imagePath, textH1, textP, textTime) {
     const main = document.getElementById('main');
