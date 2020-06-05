e4a837b00d73a768c6f043c83af63c0005581a2d
Mateus Cunha <mtscnh2@gmail.com>
2020-06-04 23:37:38 -0300

    added "Babel" and "Webpack dev server"

--- frontend/src/js/header.js
+++ frontend/src/js/header.js
@@ -1,23 +1,28 @@
-let menu = document.getElementById('menu');
-let navBar = document.getElementById('navbar');
-menu.addEventListener('click', () => {
-    document.body.style.overflow = 'hidden';
-    navBar.style.visibility = 'visible';
-});
+function eventsHeader() {
+    alert('123')
+    let menu = document.getElementById('menu');
+    let navBar = document.getElementById('navbar');
+    menu.addEventListener('click', () => {
+        document.body.style.overflow = 'hidden';
+        navBar.style.visibility = 'visible';
+    });
 
-navBar.addEventListener('click', () => {
-    navBar.style.visibility = 'hidden';
-    document.body.style.overflow = 'visible';
-});
+    navBar.addEventListener('click', () => {
+        navBar.style.visibility = 'hidden';
+        document.body.style.overflow = 'visible';
+    });
 
-let inputSearch = document.getElementById('inputSearch');
+    let inputSearch = document.getElementById('inputSearch');
 
-inputSearch.addEventListener('focus', () => {
-    let search = document.getElementById('search');
-    search.style.width = '230px';
-})
+    inputSearch.addEventListener('focus', () => {
+        let search = document.getElementById('search');
+        search.style.width = '230px';
+    })
 
-inputSearch.addEventListener('blur', () => {
-    let search = document.getElementById('search');
-    search.style.width = '130px';
-})
+    inputSearch.addEventListener('blur', () => {
+        let search = document.getElementById('search');
+        search.style.width = '130px';
+    })
+}
+
+export default eventsHeader;
--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,3 +1,6 @@
+import eventsHeader from './header.js';
+eventsHeader();
+
 let newsWrap = document.getElementsByClassName('newsWrap');
 
 for (let index = 0; index < newsWrap.length; index++) {
@@ -9,14 +12,6 @@ for (let index = 0; index < newsWrap.length; index++) {
     })
 }
 
-let imgPost = document.querySelectorAll('#imgPost');
-imgPost[0].style.background = "url('images/news1.jpg') no-repeat center";
-imgPost[0].style.backgroundSize = 'contain';
-imgPost[1].style.background = "url('images/news2.jpg') no-repeat center";
-imgPost[1].style.backgroundSize = 'contain';
-imgPost[2].style.background = "url('images/news3.jpg') no-repeat center";
-imgPost[2].style.backgroundSize = 'contain';
-
 function createNews(imagePath, textH1, textP, textTime) {
     const main = document.getElementById('main');
     let postWrap = document.createElement('div');
@@ -25,17 +20,23 @@ function createNews(imagePath, textH1, textP, textTime) {
     let imgPost = document.createElement('div');
     imgPost.setAttribute('id', 'imgPost');
     imgPost.setAttribute('style', "background: url(" + imagePath + ") center center / contain no-repeat");
+    imgPost.addEventListener('click', newsClicked);
     postWrap.appendChild(imgPost);
 
     let textPost = document.createElement('div');
     textPost.setAttribute('id', 'textPost');
+
     let title = document.createElement('h1');
+    title.addEventListener('click', newsClicked);
     title.innerHTML = textH1;
+
     let abstract = document.createElement('p');
     abstract.innerHTML = textP;
+
     let time = document.createElement('p');
     time.setAttribute('id', 'time');
     time.innerHTML = textTime;
+
     textPost.appendChild(title);
     textPost.appendChild(abstract);
     textPost.appendChild(time);
@@ -44,5 +45,12 @@ function createNews(imagePath, textH1, textP, textTime) {
     main.appendChild(postWrap);
 }
 
-createNews('images/news2.jpg', 'Tradutor da bexiga', 'pois é', '50 minutos');
+function newsClicked(event) {
+    console.log(event.path[1].children[0].innerHTML);
 
+    window.location.assign("news.html");
+}
+
+createNews('./assets/news1.jpg', 'EUA devem impedir entrada de brasileiros, diz conselheiro de Trump', "Esperamos que seja temporária', afirmou Robert O'Brien a canal de TV americano.", 'Há 50 minutos - Em mundo');
+createNews('./assets/news2.jpg', 'Toffoli é internado, passa por cirurgia e apresenta sintomas de Covid-19', "Presidente do STF passou por drenagem de abscesso e respira sem aparelhos, segundo nota do Supremo.", 'Há 50 minutos - Em mundo');
+createNews('./assets/news3.jpg', 'Secretário de vigilância do Ministério da Saúde anuncia que deixa cargo amanhã', "Wanderson de Oliveira defende o isolamento social. Ministério não anunciou sucessor.", 'Há 50 minutos - Em mundo');
